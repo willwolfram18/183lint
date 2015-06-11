6bec94d0b2b3dd0bcbdbec91f11e8bae36f702c6
Billy Wolfington <man_of_magik903@yahoo.com>
2015-06-11 18:21:06 -0400

    JS added to resize button and create more input buttons

--- static/scripts.js
+++ static/scripts.js
@@ -1,14 +1,39 @@
 $(function(){
-    function adjustInputWidth($input) {
-        var newWidth = $input.parent().innerWidth();
+    function adjustInputSize($input) {
+        var newWidth = $input.parent().innerWidth(),
+            newHeight = $input.parent().innerHeight();
         $input.width(newWidth);
+        $input.height(newHeight);
     }
 
-    function adjustInputWidths() {
+    function adjustInputSizes() {
         $('.hidden-file-input').each(function() {
-            adjustInputWidth($(this));
+            adjustInputSize($(this));
         })
     }
 
-    adjustInputWidths();
+    // Handle events with resizing to allow the choose file buttons
+    // to keep the input tag within the button
+    adjustInputSizes();
+    $(window).resize(adjustInputSizes);
+    $('.btn.file-input-btn').resize(adjustInputSizes);
+
+    function createElement(tag, elementClass) {
+        var $newElem = $(document.createElement(tag));
+        if (elementClass)
+            $newElem.addClass(elementClass);
+        return $newElem
+    }
+    function createNewFileInput() {
+        var $container = createElement('div', 'btn btn-default input-file-btn'),
+            $text = createElement('span', 'filename'),
+            $input = createElement('input', 'hidden-file-input');
+
+        $text.text('Choose a file...');
+        $input.attr('accept', '.cpp,.h').attr('type', 'file')
+        $container.append($text).append($input);
+        $('#input-files').append($container);
+        adjustInputSize($input);
+    }
+    $('#newBtn').click(createNewFileInput);
 });
