8b441b078c40208a7331a020603216ac527b1f77
Billy Wolfington <man_of_magik903@yahoo.com>
2015-06-13 07:58:27 -0400

    Added JS to activate and deactivate the Submit button

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,5 @@
-$(function(){
+$(function() {
+    // Adjust a single hidden file input to match the height and width of its parent
     function adjustInputSize($input) {
         var newWidth = $input.parent().innerWidth(),
             newHeight = $input.parent().innerHeight();
@@ -6,6 +7,7 @@ $(function(){
         $input.height(newHeight);
     }
 
+    // Adjust all hidden file inputs to match each individual's parent's height and width
     function adjustInputSizes() {
         $('.hidden-file-input').each(function() {
             adjustInputSize($(this));
@@ -20,6 +22,7 @@ $(function(){
 
     $('#newBtn').click(createNewFileInput);
 
+    // Change text of button when a file is selected
     function handler_updateInputText() {
         var filepath = $(this).val().split('\\'),
             filename = filepath[filepath.length - 1],
@@ -28,10 +31,12 @@ $(function(){
             filename = 'Chose a file...';
         }
         $span.text(filename);
+        // Button's auto wrap the $span, therefore adjust the size of the input field
         adjustInputSize($(this));
     }
     $('.hidden-file-input').change(handler_updateInputText);
 
+    // elementClass is a space separated list of classes for the created tag
     function createElement(tag, elementClass) {
         var $newElem = $(document.createElement(tag));
         if (elementClass)
@@ -39,7 +44,7 @@ $(function(){
         return $newElem
     }
     function createNewFileInput() {
-        var $container = createElement('div', 'btn btn-default input-file-btn'),
+        var $container = createElement('div', 'btn btn-default file-input-btn'),
             $text = createElement('span', 'filename'),
             $input = createElement('input', 'hidden-file-input');
 
@@ -48,6 +53,25 @@ $(function(){
         $container.append($text).append($input);
         $('#newBtn').before($container).before('<br>');
         adjustInputSize($input);
-        $input.change(handler_updateInputText)
+        // Apply the handlers to new elements that appear
+        $input.change(handler_updateInputText).change(handler_activateSubmitButton);
     }
+
+    // Toggle submit button whenever at least 1 input contains a file
+    function handler_activateSubmitButton() {
+        var isActive = false;
+        $('.hidden-file-input').each(function() {
+            if ($(this).val() != '') {
+                isActive = true;
+                $('#submit-btn').removeClass('disabled');
+            }
+        })
+
+        if (!isActive) {
+            $('#submit-btn').addClass('disabled');
+        }
+    }
+    $('.hidden-file-input').change(handler_activateSubmitButton);
+
+
 });
