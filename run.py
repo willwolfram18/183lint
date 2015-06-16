7895af4d14e347975d45cd0843a8dadc1191e5db
Billy Wolfington <man_of_magik903@yahoo.com>
2015-06-16 19:00:35 -0400

    Started process of using google for login process

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_googlelogin import GoogleLogin
 import os.path
 from werkzeug import secure_filename
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = ''
+app.config['GOOGLE_LOGIN_CLIENT_ID'] = ''
+app.config['GOOGLE_LOGIN_CLIENT_SECRET'] = ''
+app.config['GOOGLE_LOGIN_REDIRECT_URI'] = 'http://localhost:5000/oauth2callback'
+google = GoogleLogin(app)
+
 app.config['DEBUG'] = True
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 
@@ -10,7 +18,7 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', loginUrl=google.login_url(prompt='select_account', params=dict(next=url_for('about'))))
 
 @app.route('/about')
 def about():
@@ -36,7 +44,10 @@ def gradeFiles():
     print 'The following files were saved: {}'.format(savedFiles)
     return 'Success'
 
-
+@app.route('/oauth2callback')
+@google.oauth2callback
+def callback(token, userinfo, **params):
+    return redirect(url_for('about'))
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'])
